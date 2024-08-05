4b430ec4a01c40f3c0d8e998b5e202bf5c7ef7f4
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-08-06 00:17:27 +0500

    seat view created commit

--- Sittings_Views/src/main/java/com/example/sittings_views/CustomSeatVideo.java
+++ Sittings_Views/src/main/java/com/example/sittings_views/CustomSeatVideo.java
@@ -1,4 +1,13 @@
 package com.example.sittings_views;
 
-public class CustomSeatVideo {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomSeatVideo extends View {
+    public CustomSeatVideo(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
