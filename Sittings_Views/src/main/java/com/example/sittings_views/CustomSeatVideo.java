3c4a83409b87286120170bac3193799a10708ee9
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-08-08 22:51:11 +0500

    seat view created commit

--- Sittings_Views/src/main/java/com/example/sittings_views/CustomSeatVideo.java
+++ Sittings_Views/src/main/java/com/example/sittings_views/CustomSeatVideo.java
@@ -1,14 +1,50 @@
 package com.example.sittings_views;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class CustomSeatVideo extends View {
+import java.util.ArrayList;
+import java.util.List;
 
-    public CustomSeatVideo(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+public class CustomSeatVideo extends AppCompatActivity {
+
+    ViewGroup layout;
+    //
+    //theater seat structure
+    //U means booked
+    //A means empty
+    //R means reserved
+    // __ means space
+    // / means new line
+    String seats = "_UUUUUUAAAAARRRR_/"
+            + "_________________/"
+            + "UU__AAAARRRRR__RR/"
+            + "UU__UUUAAAAAA__AA/"
+            + "AA__AAAAAAAAA__AA/"
+            + "AA__AARUUUURR__AA/"
+            + "UU__UUUA_RRRR__AA/"
+            + "AA__AAAA_RRAA__UU/"
+            + "AA__AARR_UUUU__RR/"
+            + "AA__UUAA_UURR__RR/"
+            + "_________________/"
+            + "UU_AAAAAAAUUUU_RR/"
+            + "RR_AAAAAAAAAAA_AA/"
+            + "AA_UUAAAAAUUUU_AA/"
+            + "AA_AAAAAAUUUUU_AA/"
+            + "_________________/";
+
+    int seatSize = 100;
+    int seatGaping = 10;
+    int STATUS_AVAILABLE = 1;
+    int STATUS_BOOKED = 2;
+    int STATUS_RESERVED = 3;
+    String selectedIds = "";
+    List<TextView> seatViewList = new ArrayList<TextView>();
+
 }
