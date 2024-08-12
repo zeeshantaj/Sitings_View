075a47e9097c300ba115ca943012311c0d86c13d
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-08-12 23:24:57 +0500

    seat view created commit

--- app/src/main/java/com/example/sitings_view/MainActivity.java
+++ app/src/main/java/com/example/sitings_view/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomSeatView
+
     }
 }
