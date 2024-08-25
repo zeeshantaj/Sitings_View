4b9dc23bb5fc2790098ef333e059b602b0a293e1
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-08-25 22:17:49 +0500

    seat view created commit

--- Sittings_Views/src/main/java/com/example/sittings_views/SeatsType.java
+++ Sittings_Views/src/main/java/com/example/sittings_views/SeatsType.java
@@ -3,6 +3,7 @@ package com.example.sittings_views;
 public class SeatsType {
     private int THEATER_VIEW;
     private int Stadium_VIEW;
+    private int PLANE_VIEW;
     private void SetType(){
 
     }
