1cc574651b1ce852ba02e07ab77008aac99a400b
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-09-09 23:41:32 +0500

    seat view created commit

--- app/src/main/java/com/example/sitings_view/MainActivity.java
+++ app/src/main/java/com/example/sitings_view/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.sitings_view;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sittings_views.CustomSeatView;
 
@@ -16,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomSeatView customSeatView = new CustomSeatView(this);
+        customSeatView.setSeAtType(2);
 
     }
 }
