2d7a06922670bbd8c93f0c1ad8bf081c7a50b56e
zeeshan taj <zeeshantaj.zt@gmail.com>
2024-09-22 22:56:04 +0500

    set seatType success

--- Sittings_Views/src/main/java/com/example/sittings_views/CustomerSeatView.java
+++ Sittings_Views/src/main/java/com/example/sittings_views/CustomerSeatView.java
@@ -204,23 +204,6 @@ public class  CustomerSeatView extends View implements View.OnClickListener {
     private Button confirmBtn;
     private OnSeatSelectedListener listener;
 
-//    String seats = "_UUUUUUAAAAARRRR_/"
-//            + "_________________/"
-//            + "UU__AAAARRRRR__RR/"
-//            + "UU__UUUAAAAAA__AA/"
-//            + "AA__AAAAAAAAA__AA/"
-//            + "AA__AARUUUURR__AA/"
-//            + "UU__UUUA_RRRR__AA/"
-//            + "AA__AAAA_RRAA__UU/"
-//            + "AA__AARR_UUUU__RR/"
-//            + "AA__UUAA_UURR__RR/"
-//            + "_________________/"
-//            + "UU_AAAAAAAUUUU_RR/"
-//            + "RR_AAAAAAAAAAA_AA/"
-//            + "AA_UUAAAAAUUUU_AA/"
-//            + "AA_AAAAAAUUUUU_AA/"
-//            + "_________________/";
-
     List<TextView> seatViewList = new ArrayList<>();
     int seatSize = 100;
     int seatGaping = 10;
