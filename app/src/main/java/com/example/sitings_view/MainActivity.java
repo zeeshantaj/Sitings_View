4f3b6e0a315e58603051e9730cb96db15c3e87db
zeeshan taj <zeeshantaj.zt@gmail.com>
2024-09-25 23:34:52 +0500

    set seatType success

--- app/src/main/java/com/example/sitings_view/MainActivity.java
+++ app/src/main/java/com/example/sitings_view/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.sitings_view;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.LinearLayout;
+
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
