83c245b153689ac3e82e3588b25342f352b83321
zeeshan <zeeshantaj.zt@gmail.com>
2024-10-19 20:34:10 +0500

    seat changes

--- app/src/main/java/com/example/sitings_view/MainActivity.java
+++ app/src/main/java/com/example/sitings_view/MainActivity.java
@@ -13,6 +13,7 @@ import com.example.sittings_views.SeatsType;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
